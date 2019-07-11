d04d4deccc8567e751f48df0b7fbc7c3f3278523
megashrieks <shrikanthbudya@gmail.com>
2019-07-11 17:37:54 +0530

    enabled the send and disconnect event

--- socket_handler/index.js
+++ socket_handler/index.js
@@ -1,6 +1,6 @@
 module.exports = on_line_register => socket => {
     console.log("socket connected",socket.id)
     socket.on("register_connection",require("./register_connection")({socket,on_line_register}))
-    // socket.on("send", require("./send_message")({socket,on_line_register}))
-    // socket.on("disconnect", require("./deregister_connection")({socket,on_line_register}))
+    socket.on("send", require("./send_message")({socket,on_line_register}))
+    socket.on("disconnect", require("./deregister_connection")({socket,on_line_register}))
 }
